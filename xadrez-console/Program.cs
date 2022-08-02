71de7cbea3aea3e1cbf95dd16d4a1711f002a1a2
Julio Cesar <juliocesarap.df@gmail.com>
2022-08-01 21:30:30 -0300

    Criando classe posicao xadrez

--- xadrez-console/Program.cs
+++ xadrez-console/Program.cs
@@ -1,7 +1,7 @@
 ﻿using tabuleiro;
 using xadrez_console;
 using xadrez;
-try
+/*try
 {
 
     Tabuleiro tab = new Tabuleiro(8, 8);
@@ -19,3 +19,12 @@ catch (TabuleiroException e)
 {
     Console.WriteLine(e.Message);
 }
+*/
+
+PosicaoXadrez pos = new PosicaoXadrez('c',7);
+
+Console.WriteLine(pos);
+
+Console.WriteLine(pos.toPosicao());
+
+Console.ReadLine();
